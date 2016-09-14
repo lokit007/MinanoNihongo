379cf00f1886631506ff8b3667a3320717053f0a
Nhan <it007bk@gmail.com>
2016-09-14 07:26:45 +0700

    Update 14/09/2016
    Cập nhật thêm ngữ pháp

--- GiaoTrinh/ThemNguPhap.Designer.vb
+++ GiaoTrinh/ThemNguPhap.Designer.vb
@@ -24,26 +24,31 @@ Partial Class ThemNguPhap
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ThemNguPhap))
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.cbBaiHoc = New System.Windows.Forms.ComboBox()
         Me.rtbCauTruc = New System.Windows.Forms.RichTextBox()
         Me.rtbNguNghia = New System.Windows.Forms.RichTextBox()
         Me.rtbCachDung = New System.Windows.Forms.RichTextBox()
-        Me.lbViDu = New System.Windows.Forms.ListBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.tbViDu = New System.Windows.Forms.TextBox()
-        Me.tbNguNghia = New System.Windows.Forms.TextBox()
-        Me.btnThemViDu = New System.Windows.Forms.Button()
-        Me.btnXoa = New System.Windows.Forms.Button()
-        Me.btnLamTrong = New System.Windows.Forms.Button()
         Me.btnOpenFile = New System.Windows.Forms.Label()
         Me.tbAmDoc = New System.Windows.Forms.TextBox()
         Me.btnNgheLai = New System.Windows.Forms.Button()
         Me.btnLuuDung = New System.Windows.Forms.Button()
         Me.btnGhiAm = New System.Windows.Forms.Button()
+        Me.btnLamTrong = New System.Windows.Forms.Button()
+        Me.btnXoa = New System.Windows.Forms.Button()
+        Me.btnThemViDu = New System.Windows.Forms.Button()
+        Me.tbNguNghia = New System.Windows.Forms.TextBox()
+        Me.tbViDu = New System.Windows.Forms.TextBox()
         Me.btnThemNguPhap = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.dgvViDu = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewImageColumn()
         Me.GroupBox1.SuspendLayout()
+        CType(Me.dgvViDu, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'cbBaiHoc
@@ -84,18 +89,6 @@ Partial Class ThemNguPhap
         Me.rtbCachDung.Text = "Cách dùng ..."
         Me.ToolTip1.SetToolTip(Me.rtbCachDung, "Trường hợp sử dụng")
         '
-        'lbViDu
-        '
-        Me.lbViDu.Font = New System.Drawing.Font("Times New Roman", 13.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lbViDu.FormattingEnabled = True
-        Me.lbViDu.ItemHeight = 20
-        Me.lbViDu.Location = New System.Drawing.Point(458, 22)
-        Me.lbViDu.Name = "lbViDu"
-        Me.lbViDu.ScrollAlwaysVisible = True
-        Me.lbViDu.Size = New System.Drawing.Size(397, 264)
-        Me.lbViDu.TabIndex = 2
-        Me.ToolTip1.SetToolTip(Me.lbViDu, "Ví dụ minh họa")
-        '
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.btnOpenFile)
@@ -114,57 +107,6 @@ Partial Class ThemNguPhap
         Me.GroupBox1.TabIndex = 3
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Ví dụ minh họa"
-        '
-        'tbViDu
-        '
-        Me.tbViDu.Font = New System.Drawing.Font("Times New Roman", 13.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbViDu.Location = New System.Drawing.Point(10, 22)
-        Me.tbViDu.Multiline = True
-        Me.tbViDu.Name = "tbViDu"
-        Me.tbViDu.Size = New System.Drawing.Size(376, 51)
-        Me.tbViDu.TabIndex = 0
-        Me.tbViDu.Text = "Ví dụ ..."
-        Me.ToolTip1.SetToolTip(Me.tbViDu, "Nhập ví dụ minh họa")
-        '
-        'tbNguNghia
-        '
-        Me.tbNguNghia.Location = New System.Drawing.Point(10, 112)
-        Me.tbNguNghia.Multiline = True
-        Me.tbNguNghia.Name = "tbNguNghia"
-        Me.tbNguNghia.Size = New System.Drawing.Size(376, 80)
-        Me.tbNguNghia.TabIndex = 0
-        Me.tbNguNghia.Text = "Ngữ nghĩa ví dụ ..."
-        Me.ToolTip1.SetToolTip(Me.tbNguNghia, "Ngữ nghĩa của ví dụ")
-        '
-        'btnThemViDu
-        '
-        Me.btnThemViDu.Location = New System.Drawing.Point(299, 198)
-        Me.btnThemViDu.Name = "btnThemViDu"
-        Me.btnThemViDu.Size = New System.Drawing.Size(87, 27)
-        Me.btnThemViDu.TabIndex = 1
-        Me.btnThemViDu.Text = "Thêm mới"
-        Me.ToolTip1.SetToolTip(Me.btnThemViDu, "Thêm ví dụ mới")
-        Me.btnThemViDu.UseVisualStyleBackColor = True
-        '
-        'btnXoa
-        '
-        Me.btnXoa.Location = New System.Drawing.Point(204, 198)
-        Me.btnXoa.Name = "btnXoa"
-        Me.btnXoa.Size = New System.Drawing.Size(87, 27)
-        Me.btnXoa.TabIndex = 1
-        Me.btnXoa.Text = "Xóa VD"
-        Me.ToolTip1.SetToolTip(Me.btnXoa, "Xóa ví dụ được chọn")
-        Me.btnXoa.UseVisualStyleBackColor = True
-        '
-        'btnLamTrong
-        '
-        Me.btnLamTrong.Location = New System.Drawing.Point(111, 198)
-        Me.btnLamTrong.Name = "btnLamTrong"
-        Me.btnLamTrong.Size = New System.Drawing.Size(87, 27)
-        Me.btnLamTrong.TabIndex = 1
-        Me.btnLamTrong.Text = "Làm trống"
-        Me.ToolTip1.SetToolTip(Me.btnLamTrong, "Làm trống các trường dữ liệu")
-        Me.btnLamTrong.UseVisualStyleBackColor = True
         '
         'btnOpenFile
         '
@@ -216,14 +158,65 @@ Partial Class ThemNguPhap
         Me.ToolTip1.SetToolTip(Me.btnGhiAm, "Ghi âm ví dụ này")
         Me.btnGhiAm.UseVisualStyleBackColor = True
         '
+        'btnLamTrong
+        '
+        Me.btnLamTrong.Location = New System.Drawing.Point(111, 198)
+        Me.btnLamTrong.Name = "btnLamTrong"
+        Me.btnLamTrong.Size = New System.Drawing.Size(87, 27)
+        Me.btnLamTrong.TabIndex = 1
+        Me.btnLamTrong.Text = "Làm trống"
+        Me.ToolTip1.SetToolTip(Me.btnLamTrong, "Làm trống các trường dữ liệu")
+        Me.btnLamTrong.UseVisualStyleBackColor = True
+        '
+        'btnXoa
+        '
+        Me.btnXoa.Location = New System.Drawing.Point(204, 198)
+        Me.btnXoa.Name = "btnXoa"
+        Me.btnXoa.Size = New System.Drawing.Size(87, 27)
+        Me.btnXoa.TabIndex = 1
+        Me.btnXoa.Text = "Xóa VD"
+        Me.ToolTip1.SetToolTip(Me.btnXoa, "Xóa ví dụ được chọn")
+        Me.btnXoa.UseVisualStyleBackColor = True
+        '
+        'btnThemViDu
+        '
+        Me.btnThemViDu.Location = New System.Drawing.Point(299, 198)
+        Me.btnThemViDu.Name = "btnThemViDu"
+        Me.btnThemViDu.Size = New System.Drawing.Size(87, 27)
+        Me.btnThemViDu.TabIndex = 1
+        Me.btnThemViDu.Text = "Thêm mới"
+        Me.ToolTip1.SetToolTip(Me.btnThemViDu, "Thêm ví dụ mới")
+        Me.btnThemViDu.UseVisualStyleBackColor = True
+        '
+        'tbNguNghia
+        '
+        Me.tbNguNghia.Location = New System.Drawing.Point(10, 112)
+        Me.tbNguNghia.Multiline = True
+        Me.tbNguNghia.Name = "tbNguNghia"
+        Me.tbNguNghia.Size = New System.Drawing.Size(376, 80)
+        Me.tbNguNghia.TabIndex = 0
+        Me.tbNguNghia.Text = "Ngữ nghĩa ví dụ ..."
+        Me.ToolTip1.SetToolTip(Me.tbNguNghia, "Ngữ nghĩa của ví dụ")
+        '
+        'tbViDu
+        '
+        Me.tbViDu.Font = New System.Drawing.Font("Times New Roman", 13.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbViDu.Location = New System.Drawing.Point(10, 22)
+        Me.tbViDu.Multiline = True
+        Me.tbViDu.Name = "tbViDu"
+        Me.tbViDu.Size = New System.Drawing.Size(376, 51)
+        Me.tbViDu.TabIndex = 0
+        Me.tbViDu.Text = "Ví dụ ..."
+        Me.ToolTip1.SetToolTip(Me.tbViDu, "Nhập ví dụ minh họa")
+        '
         'btnThemNguPhap
         '
         Me.btnThemNguPhap.BackColor = System.Drawing.Color.LimeGreen
         Me.btnThemNguPhap.Font = New System.Drawing.Font("Times New Roman", 13.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(177, Byte))
         Me.btnThemNguPhap.ForeColor = System.Drawing.Color.DarkRed
-        Me.btnThemNguPhap.Location = New System.Drawing.Point(14, 498)
+        Me.btnThemNguPhap.Location = New System.Drawing.Point(12, 498)
         Me.btnThemNguPhap.Name = "btnThemNguPhap"
-        Me.btnThemNguPhap.Size = New System.Drawing.Size(414, 33)
+        Me.btnThemNguPhap.Size = New System.Drawing.Size(416, 33)
         Me.btnThemNguPhap.TabIndex = 12
         Me.btnThemNguPhap.Text = "Thêm ngữ pháp mới"
         Me.ToolTip1.SetToolTip(Me.btnThemNguPhap, "Thêm ngữ pháp mới")
@@ -233,14 +226,64 @@ Partial Class ThemNguPhap
         '
         Me.OpenFileDialog1.FileName = "OpenFileDialog1"
         '
+        'dgvViDu
+        '
+        Me.dgvViDu.AllowUserToAddRows = False
+        Me.dgvViDu.AllowUserToDeleteRows = False
+        Me.dgvViDu.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Times New Roman", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgvViDu.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvViDu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvViDu.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3})
+        Me.dgvViDu.EnableHeadersVisualStyles = False
+        Me.dgvViDu.Location = New System.Drawing.Point(457, 22)
+        Me.dgvViDu.MultiSelect = False
+        Me.dgvViDu.Name = "dgvViDu"
+        Me.dgvViDu.RowHeadersVisible = False
+        Me.dgvViDu.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.dgvViDu.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvViDu.Size = New System.Drawing.Size(396, 264)
+        Me.dgvViDu.TabIndex = 13
+        '
+        'Column1
+        '
+        Me.Column1.FillWeight = 45.68528!
+        Me.Column1.HeaderText = "Y/N"
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 35
+        '
+        'Column2
+        '
+        Me.Column2.FillWeight = 204.4031!
+        Me.Column2.HeaderText = "Ví dụ"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        Me.Column2.Width = 325
+        '
+        'Column3
+        '
+        Me.Column3.FillWeight = 49.91162!
+        Me.Column3.HeaderText = "Xóa"
+        Me.Column3.Image = CType(resources.GetObject("Column3.Image"), System.Drawing.Image)
+        Me.Column3.Name = "Column3"
+        Me.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.Column3.ToolTipText = "Xóa ví dụ này"
+        Me.Column3.Width = 35
+        '
         'ThemNguPhap
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(870, 539)
+        Me.Controls.Add(Me.dgvViDu)
         Me.Controls.Add(Me.btnThemNguPhap)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.lbViDu)
         Me.Controls.Add(Me.rtbCachDung)
         Me.Controls.Add(Me.rtbNguNghia)
         Me.Controls.Add(Me.rtbCauTruc)
@@ -252,6 +295,7 @@ Partial Class ThemNguPhap
         Me.Text = "Thêm ngữ pháp mới"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        CType(Me.dgvViDu, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -260,7 +304,6 @@ Partial Class ThemNguPhap
     Friend WithEvents rtbCauTruc As RichTextBox
     Friend WithEvents rtbNguNghia As RichTextBox
     Friend WithEvents rtbCachDung As RichTextBox
-    Friend WithEvents lbViDu As ListBox
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents btnLamTrong As Button
     Friend WithEvents btnXoa As Button
@@ -275,4 +318,8 @@ Partial Class ThemNguPhap
     Friend WithEvents btnThemNguPhap As Button
     Friend WithEvents ToolTip1 As ToolTip
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents dgvViDu As DataGridView
+    Friend WithEvents Column1 As DataGridViewCheckBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewImageColumn
 End Class
--- GiaoTrinh/ThemNguPhap.vb
+++ GiaoTrinh/ThemNguPhap.vb
@@ -23,6 +23,11 @@ Public Class ThemNguPhap
     Private Sub ThemNguPhap_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         pathSource &= GetNewIdNguPhap() & ".wav"
 
+        cbBaiHoc.DataSource = GetListBaiHoc()
+        cbBaiHoc.DisplayMember = "TenBaiHoc"
+        cbBaiHoc.ValueMember = "IdBaiHoc"
+        cbBaiHoc.Text = "----- Thêm vào bài học -----"
+
     End Sub
 
     Private Sub btnOpenFile_Click(sender As Object, e As EventArgs) Handles btnOpenFile.Click
@@ -69,15 +74,45 @@ Public Class ThemNguPhap
     Private Sub btnLamTrong_Click(sender As Object, e As EventArgs) Handles btnLamTrong.Click
         tbViDu.Text = String.Empty
         tbNguNghia.Text = String.Empty
+        tbAmDoc.Text = String.Empty
+        pathAudioLocation = String.Empty
 
     End Sub
 
     Private Sub btnXoa_Click(sender As Object, e As EventArgs) Handles btnXoa.Click
-
+        If dgvViDu.RowCount > 0 _
+            AndAlso MsgBox("Bạn muốn xóa các ví dụ được đánh dấu ???", MsgBoxStyle.YesNo, "Xóa ví dụ !!!") = DialogResult.Yes Then
+            dgvViDu.CurrentCell = Nothing
+            For Each pt As DataGridViewRow In dgvViDu.Rows
+                If pt.Cells(0).Value = True Then
+                    lstViDuAdd.Remove(pt.Cells(1).Value)
+                    dgvViDu.Rows.Remove(pt)
+                End If
+            Next
+        End If
     End Sub
 
     Private Sub btnThemViDu_Click(sender As Object, e As EventArgs) Handles btnThemViDu.Click
-
+        Try
+            If Not String.IsNullOrEmpty(tbViDu.Text) AndAlso Not String.IsNullOrEmpty(tbNguNghia.Text) Then
+                Dim vidu As New ViDu_Object
+                vidu.noiDung = tbViDu.Text
+                vidu.nguNghia = tbNguNghia.Text
+                vidu.urlAmDoc = tbAmDoc.Text
+                Try
+                    lstViDuAdd.Add(tbViDu.Text, vidu)
+                    If Not String.IsNullOrEmpty(pathAudioLocation) Then
+                        SaveFile(pathAudioLocation, pathSource.Replace("wav", pathAudioLocation.Substring(pathAudioLocation.LastIndexOf(".") + 1)))
+                    End If
+                    AddNewRowGridView(tbViDu.Text)
+                Catch ex As Exception
+                    MsgBox(ex.ToString)
+                End Try
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+            Throw
+        End Try
     End Sub
 
     Private Sub btnThemNguPhap_Click(sender As Object, e As EventArgs) Handles btnThemNguPhap.Click
@@ -116,6 +151,25 @@ Public Class ThemNguPhap
         End Select
     End Sub
 
+    Private Sub dgvViDu_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvViDu.CellClick
+        Try
+            If e.ColumnIndex = 2 AndAlso e.RowIndex <> -1 Then
+                lstViDuAdd.Remove(dgvViDu.CurrentRow.Cells(1).Value)
+                dgvViDu.Rows.RemoveAt(e.RowIndex)
+
+            ElseIf e.ColumnIndex = 0 AndAlso e.RowIndex = -1 Then
+                dgvViDu.CurrentCell = Nothing
+                If dgvViDu.RowCount > 0 Then
+                    For Each pt As DataGridViewRow In dgvViDu.Rows
+                        pt.Cells(0).Value = Not pt.Cells(0).Value
+                    Next
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
     'Định nghĩa dùng chung
     Private Function GetNewIdNguPhap() As Integer
         Dim dtResult As New DataTable
@@ -143,5 +197,58 @@ Public Class ThemNguPhap
             Throw
         End Try
     End Function
+
+    Private Function GetListBaiHoc() As DataTable
+        Dim dtResult As New DataTable
+        Dim da As New SqlDataAdapter
+        Dim cmd As New SqlCommand
+        Dim strSQl As New StringBuilder
+
+        Try
+            strSQl.Append("select * from BAIHOC ")
+
+            cmd.CommandText = strSQl.ToString
+            cmd.CommandType = CommandType.Text
+            cmd.Connection = conn
+
+            conn.Open()
+            da = New SqlDataAdapter(cmd)
+            da.Fill(dtResult)
+
+            cmd.Dispose()
+            da.Dispose()
+            conn.Close()
+
+            Return dtResult
+
+        Catch ex As Exception
+            Throw
+        End Try
+
+    End Function
+
+    Private Sub SaveFile(ByVal pathNguon As String, ByVal pathDich As String)
+        Try
+            If Not String.IsNullOrEmpty(pathNguon) AndAlso Not String.IsNullOrEmpty(pathDich) _
+                AndAlso System.IO.File.Exists(pathNguon) Then
+                If File.Exists(pathDich) Then
+                    File.Delete(pathDich)
+                End If
+                File.Copy(pathNguon, pathDich, True)
+
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+            Throw
+        End Try
+    End Sub
+
+    Private Sub AddNewRowGridView(ByVal vidu As String)
+        If Not String.IsNullOrEmpty(vidu) Then
+            Dim indexNewRow As Integer = dgvViDu.Rows.Add()
+            dgvViDu.Rows(indexNewRow).Cells(1).Value = vidu
+        End If
+    End Sub
+
 
 End Class
